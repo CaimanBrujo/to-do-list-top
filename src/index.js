b333d968f016bcfcfc4d94894a80017f1ac043e4
Caimanbrujo <186770762+CaimanBrujo@users.noreply.github.com>
2025-06-16 20:22:09 +0200

    feat: create and test ProjectManager module
    Added ProjectManager to manage all projects. Includes methods to add, retrieve, and switch projects. Successfully tested with multiple project entries in index.js.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./style.css";
 import logo from "./assets/images/logo-js.svg";
 import createTodo from "./todo";
 import createProject from "./project";
+import ProjectManager from "./projectManager";
+
 
 const img = document.createElement("img");
 img.src = logo;
@@ -9,6 +11,8 @@ img.alt = "Logo";
 img.width = 150;
 document.body.appendChild(img);
 
+
+// TEST todo.js and project.js
 const testProject = createProject("Personal");
 
 const todo1 = createTodo("Buy groceries", "Milk, bread, eggs", "2025-06-20", "high");
@@ -19,3 +23,16 @@ testProject.addTodo(todo2);
 
 console.log("Project name:", testProject.name);
 console.table(testProject.todos);
+
+
+// TEST projectManager.js
+ProjectManager.addProject("Personal");
+ProjectManager.addProject("Work");
+
+ProjectManager.setCurrentProject("Work");
+
+const current = ProjectManager.getCurrentProject();
+console.log("Current Project:", current);
+
+console.log("All Projects:");
+console.table(ProjectManager.getProjects());
---
+++ src/projectManager.js
@@ -0,0 +1,37 @@
+import createProject from "./project";
+
+const ProjectManager = (function () {
+    const projects = [];
+    let currentProject = null;
+
+    function addProject(name) {
+        const project = createProject(name);
+        projects.push(project);
+
+        if (!currentProject) currentProject = project;
+
+        return project;
+    }
+
+    function getProjects() {
+        return projects;
+    }
+
+    function getCurrentProject() {
+        return currentProject;
+    }
+
+    function setCurrentProject(name) {
+        const found = projects.find((project) => project.name === name);
+        if (found) currentProject = found;
+    }
+
+    return {
+        addProject,
+        getProjects,
+        getCurrentProject,
+        setCurrentProject,
+    };
+})();
+
+export default ProjectManager;
